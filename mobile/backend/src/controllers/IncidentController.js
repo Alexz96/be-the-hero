2809437b525b46bc2e91633046c2c06ac553858b
Alexsander Oliveira <alexsander1996@yahoo.com.br>
2021-01-11 22:04:32 -0300

    Update Mobile App
    Envio do App Mobile concluído em React Native

---
+++ mobile/backend/src/controllers/IncidentController.js
@@ -0,0 +1,59 @@
+const { join } = require('../database/connection');
+const connection = require('../database/connection');
+
+module.exports = {
+    async index(request, response) {
+        const { page = 1 } = request.query; // Se não for informada a página, por padrão busca a 1 
+
+        const [count] = await connection('incidents').count(); // Retorna um array, mas da forma que está, pega só o primeiro registro
+
+        const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .limit(5) // Limita a busca no banco por 5 incidentes
+            .offset((page - 1) * 5)
+            .select([
+                'incidents.*', 
+                'ongs.name', 
+                'ongs.email', 
+                'ongs.whatsapp', 
+                'ongs.city', 
+                'ongs.uf'
+            ]);
+
+        response.header('X-Total-Count', count['count(*)']);
+
+        return response.json(incidents);
+    },
+
+    async create(request, response) {
+        const { title, description, value } = request.body; // Desestruturação dos dados
+        const ong_id = request.headers.authorization;
+
+        const [id] = await connection('incidents').insert({ //Desestruturação do ID retornado no Result
+            title,
+            description,
+            value,
+            ong_id
+        });
+
+        return response.json({ id });
+    },
+
+    async delete(request, response) {
+        const { id } = request.params;
+        const ong_id = request.headers.authorization;
+
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first();
+
+        if(incident.ong_id != ong_id) {
+            return response.status(401).json({error: 'Operation not permited.'})
+        }
+        
+        await connection('incidents').where('id', id).delete();
+
+        return response.status(204).send();
+    }
+};
---
+++ mobile/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+// JSX (JavaScript XML) -> Para usar ReactJS dentro do HTML
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
